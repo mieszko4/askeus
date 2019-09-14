1e1f4aa3531db4849f68f5d2bd7874bbc79ed99b
miroslav <gljivar@gmail.com>
2019-09-14 21:43:36 +0200

    Add initial chat component

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import style from './style.scss';
 import GuestLayout from '../guest-layout';
 
 export default function () {
@@ -6,7 +7,9 @@ export default function () {
     <GuestLayout>
       <div className="container p-b-md p-r-md p-l-md has-text-centered">
         <hr />
-        TODO: Chatbot coming soon!
+        <iframe 
+        src="https://webchat.botframework.com/embed/askeus-bot?s=UvRyJ-7HVyc.qbtZC1m6oHCMgmZv7Ew6cz1lir3SJqf6hbVzBnOvdp4" 
+        className={style.iframe}></iframe>
       </div>
     </GuestLayout>
   );
