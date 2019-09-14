1565c12519b887bbfd08382eefe594c11146fd65
mieszko4 <mieszko4@gmail.com>
2019-09-14 16:02:52 +0200

    Remove Home

--- src/components/header.js
+++ src/components/header.js
@@ -49,9 +49,6 @@ export default class Header extends PureComponent {
               </button>
             </div>
             <div className={`navbar-menu ${open ? 'is-active' : ''}`}>
-              <Link className="navbar-item" to="/home" onClick={() => this.closeMenuBar()}>
-                Home
-              </Link>
               <Link className="navbar-item" to="/global-local-css" onClick={() => this.closeMenuBar()}>
                 Global & Local CSS
               </Link>
--- src/pages/guest.js
+++ src/pages/guest.js
@@ -1,21 +1,10 @@
 import fetch from 'universal-fetch';
 import skeleton from '../components/skeleton';
-import FeaturesImage from '../resources/img/seo/features.png';
 import CSSGlobalLocalImage from '../resources/img/seo/css-global-local.png';
 import SkeletonImage from '../resources/img/seo/skeleton-loading.png';
 import ImageOptimizationImage from '../resources/img/seo/image-optimization.png';
 
 export default [
-  {
-    path: '/home',
-    exact: true,
-    component: () => import('../components/home'),
-    seo: {
-      title: 'Home | ReactPWA Demo',
-      description: 'Feature set offered by ReactPWA with pluggable @pawjs plugins. ReactPWA is highly customizable and once can achieve anything as it is extendable',
-      image: FeaturesImage,
-    },
-  },
   {
     path: '/global-local-css',
     exact: true,
