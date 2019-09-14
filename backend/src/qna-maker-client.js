0aa55e29845ffdd684818de5bd3763dfd644d095
mieszko4 <mieszko4@gmail.com>
2019-09-15 01:42:09 +0200

    Lint

--- backend/src/qna-maker-client.js
+++ backend/src/qna-maker-client.js
@@ -1,39 +1,36 @@
-const https = require('https');
 const fetch = require('node-fetch');
 
 const qnaMakerParams = {
-    host : 'askeus.azurewebsites.net',
-    endpoint_key : "0184cde1-e730-4d8b-91bf-25b31709eedb",
-    kb : "cc7ea634-3238-45dc-8445-5bc35dcb59cf",
-    top : 1,
-    method : () => `/qnamaker/knowledgebases/${qnaMakerParams.kb}/generateAnswer`,
-}
+  host: 'askeus.azurewebsites.net',
+  endpoint_key: '0184cde1-e730-4d8b-91bf-25b31709eedb',
+  kb: 'cc7ea634-3238-45dc-8445-5bc35dcb59cf',
+  top: 1,
+  method: () => `/qnamaker/knowledgebases/${qnaMakerParams.kb}/generateAnswer`,
+};
 
-async function ask(requestQuestion){
-    
-    let question = {
-        'question': requestQuestion,
-        'top': qnaMakerParams.top
-    };
+async function ask(requestQuestion) {
+  const question = {
+    question: requestQuestion,
+    top: qnaMakerParams.top,
+  };
 
-    const content = JSON.stringify(question);
+  const content = JSON.stringify(question);
 
-    const url = "https://" + qnaMakerParams.host + qnaMakerParams.method();
-    console.log(url);
- 
-    const answer = await fetch(url, {
-            method: 'post',
-            body:    content,
-            headers : {
-                'Content-Type' : 'application/json',
-                'Content-Length' : Buffer.byteLength(content),
-                'Authorization' : 'EndpointKey ' + qnaMakerParams.endpoint_key,
-            }
-        });
+  const url = `https://${qnaMakerParams.host}${qnaMakerParams.method()}`;
 
-    return answer.json();
+  const answer = await fetch(url, {
+    method: 'post',
+    body: content,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(content),
+      Authorization: `EndpointKey ${qnaMakerParams.endpoint_key}`,
+    },
+  });
+
+  return answer.json();
 }
 
 module.exports = {
-    ask
+  ask,
 };
