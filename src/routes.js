842f8a8456911d84e0fa16d59acca1402b9c180d
mieszko4 <mieszko4@gmail.com>
2019-09-14 18:00:29 +0200

    Fix favicon

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import GuestRoutes from './pages/guest';
 import SplashScreen from './pages/splash';
 
+import AskeusIcon from './resources/img/askeus.png';
+
 export default class Routes {
   // eslint-disable-next-line
   apply(routeHandler) {
     routeHandler.setPwaSchema({
       name: 'AskEUs',
       short_name: 'AskEUs',
+      icons: [
+        {
+          src: AskeusIcon,
+          type: 'image/png',
+          sizes: '512x512',
+        },
+      ],
     });
     routeHandler.setDefaultSeoSchema({
       title: 'AskEUs',
