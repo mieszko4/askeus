207446b8cacfd62a0dadaf2c01ad6841b2112dbc
miroslav <gljivar@gmail.com>
2019-09-15 10:33:25 +0200

    Connect frontend and backend

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,15 +1,55 @@
 import * as React from 'react';
 import style from './style.scss';
 import GuestLayout from '../guest-layout';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
+
+export function componentDidMount() {
+  //addResponseMessage("Welcome to this awesome chat!");
+}
+export function handleNewUserMessage(newMessage) {
+  console.log(`New message incoming! ${newMessage}`);
+  
+  fetch(`/api/ask?question=${newMessage}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          console.log(result.answers);
+          addResponseMessage(result.answers[0].answer);
+          // this.setState({
+          //   isLoaded: true,
+          //   items: result.items
+          // });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          addResponseMessage("I am sorry, could you please rephrase that?");
+          // this.setState({
+          //   isLoaded: true,
+          //   error
+          // });
+        }
+      );
+
+  
+}
 
 export default function () {
   return (
     <GuestLayout>
       <div className="container p-b-md p-r-md p-l-md has-text-centered">
         <hr />
-        <iframe 
-        src="https://webchat.botframework.com/embed/askeus-bot?s=UvRyJ-7HVyc.qbtZC1m6oHCMgmZv7Ew6cz1lir3SJqf6hbVzBnOvdp4" 
-        className={style.iframe}></iframe>
+        <div className="App">
+          <Widget
+            handleNewUserMessage={handleNewUserMessage}
+            title="Chat to EU Demo"
+            subtitle="For now I can answer about your rights on brexit or obtaining residence in Poland."
+          />
+        </div>
       </div>
     </GuestLayout>
   );
